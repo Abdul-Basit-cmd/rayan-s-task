0c136566d22682d3dbe657ed71ff407339d7b36e
Abdul-Basit-cmd <abasit20062006@gmail.com>
2026-09-16 23:24:43 +0500

    fix vercel deployment

--- server/server.js
+++ server/server.js
@@ -1,10 +1,3 @@
-import app from './src/app.js';
-import config from './src/config/config.js';
-import connectDB from './src/config/db.js';
+import app from '../src/app.js';
 
-
-connectDB();
-
-app.listen(config.PORT, () => {
-    console.log(`Server is running on port ${config.PORT}`);
-})
+export default app;
